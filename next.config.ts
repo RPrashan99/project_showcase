12c98af059426816057574072ce5824fb106e112
RPrashan99 <RashmithaMK@gmail.com>
2026-03-30 19:56:35 +0530

    added deploy.yml

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,7 @@ const nextConfig: NextConfig = {
     output: "export",
     basePath: isProd ? `/${repoName}` : "",
     assetPrefix: isProd ? `/${repoName}/` : "",
+    trailingSlash: true,
     images: {
       unoptimized: true
     }
